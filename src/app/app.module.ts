8bb0d7d56aa3e61e4aaca67bb9b22b185acd9566
Manngold <jsw4820@gmail.com>
2020-11-18 18:09:51 +0900

    add Hero list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HerosComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [AppComponent, HerosComponent],
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './shared/hero.model';
+import { HEROES } from './shared/mock-heroes';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HerosComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
